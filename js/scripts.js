1304e0b980fea4f5eb0b884efd24f7da9a27eb9e
Spencer Moody <spencer.moody@outlook.com>
2020-04-10 09:28:27 -0700

    Add landing page for index.html/create order.html

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,15 @@ Pizza.prototype.pizzaPrice = function() {
   this.price = 10;
   if (this.pizzaTopping === "premium"){
     this.price += 1.50;
-  } else if )this.pizzaTopping === "standard"){
-    this.price += .5
+  } else if (this.pizzaTopping === "standard") {
+    this.price += .5;
   }
+  if (this.pizzaSize === "small") {
+    return this.price 
+  } else if (this.pizzaSize === "medium") {
+    this.price += 2;
+  } else if (this.pizzaSize === "large") {
+    this.price += 4;
+  }
+  return this.price
 }
